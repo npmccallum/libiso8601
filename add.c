7b2babda09adf76e1bcf2cba8283c2bb1e6729d9
Nathaniel McCallum <npmccallum@redhat.com>
2017-10-19 16:12:04 -0400

    Fix time subtraction

--- add.c
+++ add.c
@@ -27,8 +27,14 @@ void iso8601_add_years(iso8601_time *time, int years)
 
 void iso8601_add_months(iso8601_time *time, int months)
 {
-    months = ((int) time->month) - 1 + months;
-    time->year += months / 12;
+    if (months < 0) {
+        int years = months / 12 - 1;
+        iso8601_add_years(time, years);
+        months -= years * 12;
+    }
+
+    months += time->month - 1;
+    iso8601_add_years(time, months / 12);
     time->month = months % 12 + 1;
 }
 
@@ -53,28 +59,52 @@ void iso8601_add_days(iso8601_time *time, int days)
 
 void iso8601_add_hours(iso8601_time *time, int hours)
 {
-    hours = ((int) time->hour) + hours;
+    if (hours < 0) {
+        int days = hours / 24 - 1;
+        iso8601_add_days(time, days);
+        hours -= days * 24;
+    }
+
+    hours += time->hour;
     iso8601_add_days(time, hours / 24);
     time->hour = hours % 24;
 }
 
 void iso8601_add_minutes(iso8601_time *time, int minutes)
 {
-    minutes = ((int) time->minute) + minutes;
+    if (minutes < 0) {
+        int hours = minutes / 60 - 1;
+        iso8601_add_hours(time, hours);
+        minutes -= hours * 60;
+    }
+
+    minutes += time->minute;
     iso8601_add_hours(time, minutes / 60);
     time->minute = minutes % 60;
 }
 
 void iso8601_add_seconds(iso8601_time *time, int seconds)
 {
-    seconds = ((int) time->second) + seconds;
+    if (seconds < 0) {
+        int minutes = seconds / 60 - 1;
+        iso8601_add_minutes(time, minutes);
+        seconds -= minutes * 60;
+    }
+
+    seconds += time->second;
     iso8601_add_minutes(time, seconds / 60);
     time->second = seconds % 60;
 }
 
 void iso8601_add_useconds(iso8601_time *time, int useconds)
 {
-    useconds = ((int) time->usecond) + useconds;
+    if (useconds < 0) {
+        int seconds = useconds / 1000000 - 1;
+        iso8601_add_seconds(time, seconds);
+        time->usecond -= seconds * 1000000;
+    }
+
+    useconds += time->usecond;
     iso8601_add_seconds(time, useconds / 1000000);
     time->usecond = useconds % 1000000;
 }
