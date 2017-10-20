f40021e5b64e29d59a6397409d1cb14341d18af3
Nathaniel McCallum <npmccallum@redhat.com>
2017-10-20 10:26:54 -0400

    Flesh out t_unparse

--- t_unparse.c
+++ t_unparse.c
@@ -1,6 +1,6 @@
 /* vim: set tabstop=8 shiftwidth=4 softtabstop=4 expandtab smarttab colorcolumn=80: */
 /**
- * Copyright: 2013 Red Hat, Inc.
+ * Copyright: 2017 Red Hat, Inc.
  * Author: Nathaniel McCallum <npmccallum@redhat.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,29 +20,243 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
-#define ARRAY_LENGTH(arr) (sizeof((arr)) / sizeof(*(arr)))
+#include <errno.h>
 
 struct {
+    iso8601_truncate truncate;
+    iso8601_format format;
+    uint8_t ydigits;
+    uint32_t flags;
+
     iso8601_time time;
     const char *str;
 } tests[] = {
-    {{1970, 6, 1, 10, 10, 10, 123456}, "1970-01-01T00:00:00Z"},
+    /* All truncations of normal format. */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-03-03T03:03:03.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-03-03T03:03:03Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-03-03T03:03Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-03-03T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-03-03" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-03" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000" },
+
+    /* All truncations of normal format (basic mode). */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "20000303T030303.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "20000303T030303Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "20000303T0303Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "20000303T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "20000303" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "200003" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000" },
+
+    /* All truncations of ordinal format. */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-063T03:03:03.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-063T03:03:03Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-063T03:03Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-063T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-063" },
+    { ISO8601_TRUNCATE_ORDINAL, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-063" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000" },
+
+    /* All truncations of ordinal format (basic mode). */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000063T030303.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000063T030303Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000063T0303Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000063T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000063" },
+    { ISO8601_TRUNCATE_ORDINAL, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000063" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_ORDINAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000" },
+
+    /* All truncations of weekdate format. */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-W09-5T03:03:03.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-W09-5T03:03:03Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-W09-5T03:03Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-W09-5T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-W09-5" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000-W09" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000" },
+
+    /* All truncations of weekdate format (basic mode). */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000W095T030303.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000W095T030303Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000W095T0303Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000W095T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000W095" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000W09" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_WEEKDATE, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "2000" },
+
+    /* All truncations of normal format with long years. */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03:03:03.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03:03:03Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03:03Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000" },
+
+    /* All truncations of normal format with long years (no basic mode). */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03:03:03.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03:03:03Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03:03Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03-03" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000-03" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_NORMAL, 6, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456 }, "002000" },
+
+    /* All truncations of normal format with negative year. */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000-03-03T03:03:03.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000-03-03T03:03:03Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000-03-03T03:03Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000-03-03T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000-03-03" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000-03" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000" },
+
+    /* All truncations of normal format (basic mode). */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-20000303T030303.123456Z" },
+    { ISO8601_TRUNCATE_SECOND, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-20000303T030303Z" },
+    { ISO8601_TRUNCATE_MINUTE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-20000303T0303Z" },
+    { ISO8601_TRUNCATE_HOUR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-20000303T03Z" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-20000303" },
+    { ISO8601_TRUNCATE_MONTH, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-200003" },
+    { ISO8601_TRUNCATE_YEAR, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { -2000, 3, 3, 3, 3, 3, 123456 }, "-2000" },
+
+    /* Timezones */
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, false, 90 },
+        "2000-03-03T03:03:03.123456+01:30" },
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456, false, 90 },
+        "20000303T030303.123456+0130" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, false, 90 },
+        "2000-03-03" },
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, false, -90 },
+        "2000-03-03T03:03:03.123456-01:30" },
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456, false, -90 },
+        "20000303T030303.123456-0130" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, false, -90 },
+        "2000-03-03" },
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, true, 0 },
+        "2000-03-03T03:03:03.123456" },
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, true, 90 }, /* Offset is ignored. */
+        "2000-03-03T03:03:03.123456" },
+    { ISO8601_TRUNCATE_NONE, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_BASIC,
+        { 2000, 3, 3, 3, 3, 3, 123456, true, 0 },
+        "20000303T030303.123456" },
+    { ISO8601_TRUNCATE_DAY, ISO8601_FORMAT_NORMAL, 4, ISO8601_FLAG_NONE,
+        { 2000, 3, 3, 3, 3, 3, 123456, true, 0 },
+        "2000-03-03" },
+
+    {}
 };
+
+static void test_e2big(iso8601_format format)
+{
+    static const iso8601_time time = { 2000, 3, 3, 3, 3, 3, 123456, false, 90 };
+
+    for (int i = 0, r = E2BIG; r == E2BIG; i++) {
+        char buf[i];
+        memset(buf, 0, i);
+        r = iso8601_unparse(&time, ISO8601_FLAG_NONE, 4, format,
+                            ISO8601_TRUNCATE_NONE, i, buf);
+        assert(r == 0 || r == E2BIG);
+    }
+}
 
 int main(int argc, const char **argv)
 {
-    char buf[1000];
-    int ret;
+    for (size_t i = 0; tests[i].str; i++) {
+        char buf[1024] = {};
 
-    ret = iso8601_unparse(&tests[0].time, ISO8601_FLAG_BASIC, 6,
-                          ISO8601_FORMAT_NORMAL, ISO8601_TRUNCATE_NONE,
-                          sizeof(buf), buf);
-    if (ret != 0) {
-        fprintf(stderr, "%s\n", strerror(ret));
-        assert(ret == 0);
+        fprintf(stderr, "answer: %s\n", tests[i].str);
+        assert(iso8601_unparse(&tests[i].time, tests[i].flags,
+                               tests[i].ydigits, tests[i].format,
+                               tests[i].truncate, sizeof(buf), buf) == 0);
+
+        fprintf(stderr, "result: %s\n", buf);
+        assert(strcmp(buf, tests[i].str) == 0);
     }
-    fprintf(stderr, "%s\n", buf);
+
+    /* Test all the buffer too small conditions. */
+    test_e2big(ISO8601_FORMAT_NORMAL);
+    test_e2big(ISO8601_FORMAT_ORDINAL);
+    test_e2big(ISO8601_FORMAT_WEEKDATE);
 
     return 0;
 }
